c5651f4a1e04ef89de919c732172705658fa6df5
Aaron Bedra <aaron@thinkrelevance.com>
2009-03-16 12:38:49 -0400

    splitting out into 1.9 folder

--- ext/rcovrt/extconf.rb
+++ ext/rcovrt/extconf.rb
@@ -3,15 +3,15 @@ unless RUBY_PLATFORM == 'java' then
 
   if RUBY_VERSION =~ /1.9/
     $CFLAGS << ' -DRUBY_19_COMPATIBILITY'
-  end
 
-  dir_config("gcov")
-  if ENV["USE_GCOV"] and Config::CONFIG['CC'] =~ /gcc/ and
-    have_library("gcov", "__gcov_open")
+    dir_config("gcov")
+    if ENV["USE_GCOV"] and Config::CONFIG['CC'] =~ /gcc/ and
+      have_library("gcov", "__gcov_open")
 
-    $CFLAGS << " -fprofile-arcs -ftest-coverage"
-    create_makefile("rcovrt")
-  else
-    create_makefile("rcovrt")
+      $CFLAGS << " -fprofile-arcs -ftest-coverage"
+      create_makefile("1.9/rcovrt")
+    else
+      create_makefile("1.9/rcovrt")
+    end
   end
 end
